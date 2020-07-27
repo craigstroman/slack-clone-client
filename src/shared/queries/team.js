6ecf24e0211d52baf9141eb4d154d6b8ff49c2ae
Craig Stroman <cstroman@gmail.com>
2020-07-26 21:35:13 -0400

    Adding a shared query

---
+++ src/shared/queries/team.js
@@ -0,0 +1,34 @@
+import gql from 'graphql-tag';
+
+const meQuery = gql`
+  query meQuery {
+    me {
+      id
+      uuid
+      username
+      teams {
+        id
+        uuid
+        name
+        admin
+        directMessageMembers {
+          id
+          uuid
+          username
+        }
+        channels {
+          id
+          uuid
+          name
+        }
+        teamMembers {
+          id
+          uuid
+          username
+        }
+      }
+    }
+  }
+`;
+
+export default meQuery;
