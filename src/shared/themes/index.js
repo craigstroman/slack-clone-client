39f565a668689c9a83cb888b9940ab895b7722d9
Craig Stroman <cstroman@gmail.com>
2020-08-03 21:45:15 -0400

    Updating theme file

--- src/shared/themes/index.js
+++ src/shared/themes/index.js
@@ -2,10 +2,10 @@ import variables from './variables';
 import mixins from './mixins';
 import colors from './colors';
 
-const theme = {
+const Themes = {
   variables,
   colors,
   mixins,
 };
 
-export default theme;
+export default Themes;
