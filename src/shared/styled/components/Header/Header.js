e29ad2f02a0611d52fcc2b3404c3e32f9c20330c
Craig Stroman <cstroman@gmail.com>
2020-11-17 17:16:22 -0500

    Adding styled folder for styled-componets variables

---
+++ src/shared/styled/components/Header/Header.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 10%;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+export const ModalContent = styled.div`
+  height: 400px;
+  overflow: scroll-y;
+  width: 500px;
+  svg {
+    font-size: inherit;
+  }
+`;
