cd7b3cafea288c1ed503f981ab1d995762a46210
Craig Stroman <cstroman@gmail.com>
2020-11-25 22:34:22 -0500

    Creating styles file for login page

---
+++ src/shared/styled/pages/Login/Login.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  margin-top: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+`;
+
+export const Content = styled.div`
+  header {
+    margin: 0 auto;
+    h1 {
+      text-align: center;
+    }
+  }
+  margin: 0 auto;
+  width: 100%;
+  form {
+    margin: 0 auto;
+    text-align: center;
+  }
+`;
+
+export const StyledTextField = styled(TextField)`
+  .MuiOutlinedInput-root {
+    text-align: left;
+    width: 450px;
+  }
+`;
+export const StyledSnackbar = styled(Snackbar)`
+  .MuiTypography-root {
+    background-color: #d32f2f;
+  }
+`;
