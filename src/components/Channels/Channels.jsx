7aabc415574bf6848bc465bba518c1b7c83d0e3e
Craig Stroman <cstroman@gmail.com>
2020-11-30 21:06:40 -0500

    Moving styles to external file

--- src/components/Channels/Channels.jsx
+++ src/components/Channels/Channels.jsx
@@ -1,63 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { Button, IconButton } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { ChannelsList, Heading, Wrapper } from '../../shared/styled/components/Channels/Channels';
 import AddChannel from '../AddChannel/AddChannel';
 import Themes from '../../shared/themes';
-
-const Wrapper = styled.div`
-  margin-top: 10px;
-`;
-
-const Heading = styled.div`
-  display: block;
-  margin: 0 auto;
-  text-align: center;
-  width: 100%;
-  h3,
-  button {
-    color: ${(props) => props.theme.colors.shadyLady};
-    float: left;
-  }
-  h3 {
-    padding-top: 20px;
-    margin-left: 10px;
-  }
-
-  ${(props) => props.theme.mixins.clearfix()}
-`;
-
-const ChannelsList = styled.ul`
-  list-style-type: none;
-  padding-left: 0;
-  li {
-    &.selected {
-      background-color: ${(props) => props.theme.colors.toryBlue};
-      &:hover,
-      &:active,
-      &:visited {
-        background-color: ${(props) => props.theme.colors.toryBlue};
-      }
-      button {
-        &:hover,
-        &:active,
-        &:visited {
-          background-color: ${(props) => props.theme.colors.toryBlue};
-        }
-      }
-    }
-    margin-left: 0;
-    padding-left: 0;
-    button {
-      color: ${(props) => props.theme.colors.shadyLady};
-      font-size: 1em;
-    }
-
-    ${(props) => props.theme.mixins.clearfix()}
-  }
-`;
 
 class Channels extends React.Component {
   constructor(props) {
