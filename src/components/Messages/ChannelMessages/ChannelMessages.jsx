f1f6f26cbdca5f2541a2d1bf01c0583ff8f238e4
Craig Stroman <cstroman@gmail.com>
2020-11-30 21:20:09 -0500

    Moving styles to external file

--- src/components/Messages/ChannelMessages/ChannelMessages.jsx
+++ src/components/Messages/ChannelMessages/ChannelMessages.jsx
@@ -4,48 +4,13 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import Moment from 'react-moment';
 import uniqid from 'uniqid';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import {
+  Message,
+  MessageHeader,
+  Wrapper,
+} from '../../../shared/styled/components/Messages/ChannelMessages/ChannelMessages';
 import Themes from '../../../shared/themes';
-
-const Wrapper = styled.div`
-  background-color: ${(props) => props.theme.colors.white};
-  color: ${(props) => props.theme.colors.black};
-  display: flex;
-  flex-direction: column-reverse;
-  height: 89vh;
-  overflow-y: scroll;
-  ul {
-    list-style-type: none;
-    margin-block-start: 0;
-    margin-block-end: 0;
-    padding-inline-start: 0;
-    li {
-      margin-bottom: 10px;
-      padding-left: 10px;
-    }
-  }
-`;
-const MessageHeader = styled.header`
-  display: block;
-  width: 100%;
-  h3 {
-    display: inline-block;
-    font-weight: bold;
-    margin-right: 20px;
-    text-align: left;
-  }
-  div {
-    color: ${(props) => props.theme.colors.scorpion};
-    display: inline-block;
-    font-size: 0.875em;
-  }
-`;
-
-const Message = styled.div`
-  display: block;
-  text-align: left;
-  width: 100%;
-`;
 
 const newChannelMessageSubscription = gql`
   subscription($channelId: Int!) {
