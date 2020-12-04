7d1cf3d5f0132973d2e8e3e11a5dd598efefaa35
Craig Stroman <cstroman@gmail.com>
2020-12-04 00:22:20 -0500

    Adding styles

--- src/shared/styled/components/PopUpMenu/PopUpMenu.js
+++ src/shared/styled/components/PopUpMenu/PopUpMenu.js
@@ -2,17 +2,38 @@ import { Link } from 'react-router-dom';
 import { Menu } from '@material-ui/core';
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  ul {
+    top: 80px;
+  }
+`;
+
 export const StyledTextLink = styled(Link)`
   color: ${(props) => props.theme.colors.black};
+  text-decoration: none;
   &:hover,
   &:focus,
   &:active {
     color: ${(props) => props.theme.colors.black};
+    text-decoration: underline;
   }
 `;
 
 export const StyledMenu = styled(Menu)`
-  .MuiMenu-paper {
-    top: 83px !important;
+  top: 60px !important;
+  .settings-menu__item {
+    padding: 20px;
+
+    button {
+      background: none;
+      color: ${(props) => props.theme.colors.black};
+      text-transform: none;
+      &:hover,
+      &:focus,
+      &:active {
+        color: ${(props) => props.theme.colors.black};
+        text-decoration: underline;
+      }
+    }
   }
 `;
