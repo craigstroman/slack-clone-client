a47fc7539a2d988c01767b193e432ff060ab98eb
Craig Stroman <cstroman@gmail.com>
2020-12-07 21:22:11 -0500

    Adding EditProfile component and me object

--- src/components/PopUpMenu/PopUpMenu.jsx
+++ src/components/PopUpMenu/PopUpMenu.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
-import { Button, MenuItem } from '@material-ui/core';
+import { Button, Menu, MenuItem } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog } from '@fortawesome/free-solid-svg-icons';
-import { StyledTextLink, StyledMenu } from '../../shared/styled/components/PopUpMenu/PopUpMenu';
+import { StyledMenu, StyledTextLink, Wrapper } from '../../shared/styled/components/PopUpMenu/PopUpMenu';
+import EditProfile from '../EditProfile/EditProfile';
 import Themes from '../../shared/themes';
 
 class PopUpMenu extends React.Component {
@@ -14,14 +15,17 @@ class PopUpMenu extends React.Component {
 
     this.state = {
       anchorEl: null,
+      settingsModal: false,
     };
 
     this.logout = this.logout.bind(this);
-    this.handleOpen = this.handleOpen.bind(this);
-    this.handleClose = this.handleClose.bind(this);
+    this.handleOpenMenu = this.handleOpenMenu.bind(this);
+    this.handleCloseMenu = this.handleCloseMenu.bind(this);
+    this.handleOpenSettingsModal = this.handleOpenSettingsModal.bind(this);
+    this.handleCloseSettingsModal = this.handleCloseSettingsModal.bind(this);
   }
 
-  handleOpen = (e) => {
+  handleOpenMenu = (e) => {
     const { currentTarget } = e;
 
     this.setState({
@@ -29,10 +33,18 @@ class PopUpMenu extends React.Component {
     });
   };
 
-  handleClose = () => {
+  handleCloseMenu = () => {
     this.setState({
       anchorEl: null,
     });
+  };
+
+  handleOpenSettingsModal = () => {
+    this.setState({ settingsModal: true });
+  };
+
+  handleCloseSettingsModal = () => {
+    this.setState({ settingsModal: false });
   };
 
   logout = () => {
@@ -45,29 +57,44 @@ class PopUpMenu extends React.Component {
   };
 
   render() {
-    const { anchorEl } = this.state;
+    const { anchorEl, settingsModal } = this.state;
+    const { me } = this.props;
 
     return (
       <>
         <ThemeProvider theme={Themes}>
-          <Button aria-controls="settings-menu" aria-haspopup="true" onClick={(e) => this.handleOpen(e)}>
-            <FontAwesomeIcon icon={faCog} className="settings-icon" />
-          </Button>
-          <StyledMenu
-            id="settings-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={this.handleClose}
-          >
-            <MenuItem onClick={this.handleClose}>
-              <StyledTextLink to="/teams">Teams</StyledTextLink>
-            </MenuItem>
-            <MenuItem onClick={this.logout}>
-              <StyledTextLink to="/">Logout</StyledTextLink>
-            </MenuItem>
-          </StyledMenu>
+          <Wrapper className="wrapper">
+            <Button
+              aria-controls="settings-menu"
+              aria-haspopup="true"
+              onClick={(e) => this.handleOpenMenu(e)}
+            >
+              <FontAwesomeIcon icon={faCog} className="settings-icon" />
+            </Button>
+            <StyledMenu
+              id="settings-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={this.handleCloseMenu}
+            >
+              <MenuItem className="settings-menu__item" onClick={this.handleCloseMenu}>
+                <StyledTextLink to="/teams">Teams</StyledTextLink>
+              </MenuItem>
+              <MenuItem className="settings-menu__item" onClick={this.handleCloseMenu}>
+                <Button onClick={this.handleOpenSettingsModal}>Edit Profile</Button>
+              </MenuItem>
+              <MenuItem className="settings-menu__item" onClick={this.handleCloseMenu}>
+                <Button onClick={this.logout}>Logout</Button>
+              </MenuItem>
+            </StyledMenu>
+          </Wrapper>
         </ThemeProvider>
+        <EditProfile
+          isOpen={settingsModal}
+          handleCloseSettingsModal={() => this.handleCloseSettingsModal()}
+          me={me}
+        />
       </>
     );
   }
@@ -75,10 +102,12 @@ class PopUpMenu extends React.Component {
 
 PopUpMenu.defaultProps = {
   history: {},
+  me: {},
 };
 
 PopUpMenu.propTypes = {
   history: PropTypes.object,
+  me: PropTypes.object,
 };
 
 export default withRouter(PopUpMenu);
