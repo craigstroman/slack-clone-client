93d82bb9d4ad99d52862589a7ac521627db9d37c
Craig Stroman <cstroman@gmail.com>
2020-12-17 22:53:13 -0500

    Moving CreateTeam component styles to external file

---
+++ src/shared/styled/components/CreateTeam/CreateTeam.js
@@ -0,0 +1,30 @@
+import { Dialog, TextField } from '@material-ui/core';
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: grid;
+  h1 {
+    text-align: center;
+  }
+  .MuiGrid-container {
+    margin: 0 auto;
+    text-align: center;
+  }
+`;
+export const StyledDialog = styled(Dialog)`
+  .MuiDialogTitle-root {
+    border-bottom: 1px solid #dee2e6;
+    margin-bottom: 1rem;
+  }
+  .MuiDialogActions-root {
+    border-top: 1px solid #dee2e6;
+    margin-top: 1rem;
+  }
+`;
+
+export const StyledTextField = styled(TextField)`
+  .MuiOutlinedInput-root {
+    text-align: left;
+    width: 500px;
+  }
+`;
