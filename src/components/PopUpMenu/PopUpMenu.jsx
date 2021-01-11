ac3fa575bc7f3b4e9a8f7ab82b57a4e9ec1eca4b
Craig Stroman <cstroman@gmail.com>
2021-01-11 16:45:44 -0500

    Code cleanup

--- src/components/PopUpMenu/PopUpMenu.jsx
+++ src/components/PopUpMenu/PopUpMenu.jsx
@@ -11,10 +11,6 @@ import EditProfile from '../EditProfile/EditProfile';
 import CreateTeam from '../CreateTeam/CreateTeam';
 import Themes from '../../shared/themes';
 
-// TODO: Continue working on making it so on the teams database it adds the user who creates a team when creating.
-// Then change team resolver so that a team is only unique based on if the same user who's the team owner has one.
-// Look at using members table for this because that has who a admin of a team is.
-
 class PopUpMenu extends React.Component {
   constructor(props) {
     super(props);
