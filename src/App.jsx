d792da2b8267cc5440d835da84bcbf71aa769f90
Craig Stroman <cstroman@gmail.com>
2021-01-14 22:17:53 -0500

    Adding Bootstrap CSS

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import registerServiceWorker from './registerServiceWorker';
 import Home from './pages/Home/Home';
 import GlobalStyles from './globalStyles';
 import client from './apollo';
+import 'bootstrap/scss/bootstrap.scss';
 
 const App = (
   <>
