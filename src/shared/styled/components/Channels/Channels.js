3872c2910022f9c04455bcd6defa09ede37c7be6
Craig Stroman <cstroman@gmail.com>
2021-01-16 12:13:15 -0500

    Removing not needed styles

--- src/shared/styled/components/Channels/Channels.js
+++ src/shared/styled/components/Channels/Channels.js
@@ -4,24 +4,6 @@ export const Wrapper = styled.div`
   margin-top: 10px;
 `;
 
-export const Heading = styled.div`
-  display: block;
-  margin: 0 auto;
-  text-align: center;
-  width: 100%;
-  h3,
-  button {
-    color: ${(props) => props.theme.colors.shadyLady};
-    float: left;
-  }
-  h3 {
-    padding-top: 20px;
-    margin-left: 10px;
-  }
-
-  ${(props) => props.theme.mixins.clearfix()}
-`;
-
 export const ChannelsList = styled.ul`
   list-style-type: none;
   padding-left: 0;
