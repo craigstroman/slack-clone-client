025e3fea91cfce8fec302f9d2ee612304da20bf3
Craig Stroman <cstroman@gmail.com>
2021-01-22 18:17:19 -0500

    Moving styles to external file

--- src/pages/Teams/NoTeams/NoTeams.jsx
+++ src/pages/Teams/NoTeams/NoTeams.jsx
@@ -1,37 +1,25 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
-import styled from 'styled-components';
+import Wrapper from '../../../shared/styled/pages/Teams/NoTeams/NotTeams';
 import PopUpMenu from '../../../components/PopUpMenu/PopUpMenu';
-
-const Wrapper = styled.div`
-  margin: 0 auto;
-  width: 100%;
-  header {
-    h1 {
-      text-align: center;
-    }
-  }
-  main {
-    margin: 0 auto;
-    text-align: center;
-  }
-`;
 
 const NoTeams = (props) => (
   <Wrapper>
     <header>
       <Row>
-        <Col md={12}>
-          <h1>Slack Clone - No Teams</h1>
+        <Col md={12} className="header-text">
+          <h1>Slack Clone - Create A Team</h1>
           <hr />
+        </Col>
+      </Row>
+      <Row>
+        <Col md={12} className="pop-up-menu">
+          <PopUpMenu />
         </Col>
       </Row>
     </header>
     <main>
       <Row>
-        <Col md={12}>
-          <PopUpMenu />
-        </Col>
         <Col md={12}>
           You have no teams. &nbsp; You either need to <a href="/create-team">create a team</a>&nbsp; or be
           invited to one.
