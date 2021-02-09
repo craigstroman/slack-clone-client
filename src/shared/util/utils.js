acd02eb2ce5fb4568d6c1ada88e8fa6e874cbd37
Craig Stroman <cstroman@gmail.com>
2021-02-09 17:01:01 -0500

    Updating and adding documentation for functions

--- src/shared/util/utils.js
+++ src/shared/util/utils.js
@@ -1,8 +1,8 @@
 /**
  * Validates a email.
  *
- * @param      {String}   email   The email
- * @return     {boolean}  { description_of_the_return_value }
+ * @param      {String}   email   The email.
+ * @return     {boolean}  True/false status of input.
  */
 export const validateEmail = (email) => {
   const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -14,6 +14,12 @@ export const validateEmail = (email) => {
   return false;
 };
 
+/**
+ * Validates a phone number.
+ *
+ * @param {String} phoneNumber  The phone number.
+ * @return     {boolean}  True/false status of input.
+ */
 export const validatePhoneNumber = (phoneNumber) => {
   const phoneRex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 
