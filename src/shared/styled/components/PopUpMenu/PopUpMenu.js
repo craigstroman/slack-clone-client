1a4f93edfd18c86669c354c4f0341b66a7b3821f
Craig Stroman <cstroman@gmail.com>
2021-02-09 23:15:33 -0500

    Adding new styles

--- src/shared/styled/components/PopUpMenu/PopUpMenu.js
+++ src/shared/styled/components/PopUpMenu/PopUpMenu.js
@@ -1,6 +1,26 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  .dropdown {
+    .dropdown-toggle {
+      background: transparent !important;
+      border: 1px solid transparent;
+      box-shadow: none !important;
+      outline: none;
+      &:focus,
+      &:hover,
+      &:active,
+      &:visited {
+        background: transparent !important;
+        border: 1px solid transparent !important;
+        box-shadow: none !important;
+        outline: none;
+      }
+      &:after {
+        content: none;
+      }
+    }
+  }
   .dropdown-menu {
     .dropdown-item {
       .btn {
@@ -13,6 +33,17 @@ const Wrapper = styled.div`
         }
       }
     }
+  }
+
+  .user-icon {
+    background-color: ${(props) => props.theme.colors.treePoppy};
+    border-radius: 50%;
+    color: ${(props) => props.theme.colors.white};
+    line-height: 50px;
+    text-align: center;
+    font-size: 1.5em;
+    height: 50px;
+    width: 50px;
   }
 `;
 
