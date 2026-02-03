4402a3a33de2e475018b513f77d00de49b3ee3e1
Craig Stroman <cstroman@gmail.com>
2026-02-02 20:53:25 -0500

    Deleting old pages

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Client, Provider, fetchExchange, ssrExchange } from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
+import gql from 'graphql-tag';
+import { Main } from './pages/Main/Main'
+import { betterUpdateQuery } from './shared/utils/betterUpdateQuery';
+// dedupExchange
+
+const element = document.getElementById('app');
+const root = createRoot(element as HTMLDivElement);
+const nodeEnv = process.env.NODE_ENV;
+const ssr = ssrExchange({ isClient: false });
+
+const client = new Client({
+  url:
+    nodeEnv === 'production' ? 'https://lireddit.craigstroman.com/graphql' : 'http://localhost:9000/graphql',
+  fetchOptions: {
+    credentials: 'include',
+  },
+  exchanges: [
+    cacheExchange({
+      keys: {
+      },
+      resolvers: {
+        Query: {
+        },
+      },
+      updates: {
+        Mutation: {
+          logout: (_result, args, cache, info) => {
+            betterUpdateQuery<LogoutMutation, MeQuery>(cache, { query: MeDocument }, _result, () => ({
+              me: null,
+            }));
+            cache.invalidate('Query', 'Me');
+          },
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result: any, query: any) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              },
+            );
+            invalidateAllPosts(cache);
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result: any, query: any) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              },
+            );
+          },
+        },
+      },
+    }),
+    debugExchange,
+    fetchExchange,
+    ssr,
+    error,
+  ],
+});
+
+root.render(
+  <React.StrictMode>
+    <Provider value={client}>
+      <Main />
+    </Provider>
+  </React.StrictMode>,
+);
