34b28e6a00384db424481fc6ab760a1f36625531
Craig Stroman <cstroman@gmail.com>
2026-02-07 14:32:36 -0500

    Updating interfaces

--- src/shared/Interfaces.ts
+++ src/shared/Interfaces.ts
@@ -34,6 +34,7 @@ export interface IRegisterFormValues {
   first_name: string;
   last_name: string;
   email: string;
+  phone_number: string;
   username: string;
   password: string;
   password_confirmation: string;
