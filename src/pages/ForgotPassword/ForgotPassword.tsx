9cb6354fc7e7b8e9f61bdd9a71584f7ce8135f3b
Craig Stroman <cstroman@gmail.com>
2026-02-08 13:47:44 -0500

    Creating forgot password page

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,5 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import { InputField } from '../../components/InputFIeld/InputField';
+import { IForgotPassword } from '../../shared/Interfaces';
+import { useForgotPasswordMutation, useMeQuery } from '../../generated/graphql';
+import './ForgotPassword.scss';
 
 export const ForgotPassword: React.FC = () => {
-  return <div>Forgot password page</div>;
+  const [{ data }] = useMeQuery();
+  const [complete, setComplete] = useState<boolean>(false);
+  const [linkURL, setLinkURL] = useState<string>('');
+  const initialValues: IForgotPassword = {
+    email: '',
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email('Enter a valid email.').required('Email is required.'),
+  });
+
+  const [, forgotPassword] = useForgotPasswordMutation();
+  return (
+    <div className="forgot-password-container">
+      <h1>Forgot Password</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await forgotPassword(values);
+          setComplete(true);
+          if (response && response.data) {
+            setLinkURL(response.data.forgotPassword);
+          }
+        }}
+        validationSchema={validationSchema}
+      >
+        {({ errors }) => {
+          if (complete) {
+            return (
+              <div className="content">
+                <a href={linkURL}>Change Password</a>
+              </div>
+            );
+          } else {
+            return (
+              <Form>
+                <div className="form-row">
+                  <InputField name="email" placeholder="Enter a email" fieldErrors={errors} type="email" />
+                </div>
+                <div className="form-row">
+                  <button type="submit" className="button">
+                    Send Email
+                  </button>
+                </div>
+              </Form>
+            );
+          }
+        }}
+      </Formik>
+    </div>
+  );
 };
