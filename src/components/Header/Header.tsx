0b61ffaa0c4d838664a76ff6793b4a56d284562b
Craig Stroman <cstroman@gmail.com>
2026-02-16 17:47:12 -0500

    Updating header markup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import './Header.scss';
 
 export const Header: React.FC = () => {
-  return <div className="header-container">Header</div>;
+  return (
+    <div className="header-container">
+      <h1>Header</h1>
+    </div>
+  );
 };
