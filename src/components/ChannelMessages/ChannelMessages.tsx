3f32b52d9652e9d334b1379798780b5f2538934e
Craig Stroman <cstroman@gmail.com>
2026-02-16 16:26:30 -0500

    Creating channel messages component

---
+++ src/components/ChannelMessages/ChannelMessages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ChannelMessages.scss';
+
+export const ChannelMessages: React.FC = () => {
+  return (
+    <div className="channel-message-container">
+      <div className="channel-message-header">
+        <h1>Channel Messages</h1>
+      </div>
+    </div>
+  );
+};
