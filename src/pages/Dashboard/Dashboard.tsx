3e1952df3dcebfc70eb9c94c5c878ec6c1cdb993
Craig Stroman <cstroman@gmail.com>
2026-02-15 20:16:15 -0500

    Adding security to dashboard page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useMeQuery } from '../../generated/graphql';
+import './Dashboard.scss';
 
 export const Dashboard: React.FC = () => {
+  const navigate = useNavigate();
+  const [{ data, fetching: meLoading }] = useMeQuery();
+
+  console.log('data: ', data);
+
+  useEffect(() => {
+    if (!meLoading) {
+      if (data && data.me && !data?.me.username) {
+        navigate('/');
+      }
+    }
+  }, [data, meLoading]);
+
   return <div>Dashboard</div>;
 };
