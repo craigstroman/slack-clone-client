098b5a518b54d8112815b6cca3faa8613646b5d3
Craig Stroman <cstroman@gmail.com>
2026-02-16 17:58:42 -0500

    Updating footer markup

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import './Footer.scss';
 
 export const Footer: React.FC = () => {
-  return <div className="footer-container">Footer</div>;
+  return (
+    <div className="footer-container">
+      <h1>Footer</h1>
+    </div>
+  );
 };
