1597489e2a5b78ad99166f34ffb29ae30966650f
Craig Stroman <cstroman@gmail.com>
2026-03-15 21:42:25 -0400

    Adding to sidebar markup

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import './SideBar.scss';
 
 export const SideBar: React.FC = () => {
-  return <div className="sidebar-container">Sidebar</div>;
+  return (
+    <div className="sidebar-container">
+      <div className="sidebar-channels">Channels</div>
+    </div>
+  );
 };
