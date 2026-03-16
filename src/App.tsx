972cecbafb50c9ad4847c4e782f08d84c50e7228
Craig Stroman <cstroman@gmail.com>
2026-03-16 14:42:19 -0400

    Removing a not needed import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import gql from 'graphql-tag';
 import { createRoot } from 'react-dom/client';
 import { Client, Provider, fetchExchange, ssrExchange } from 'urql';
 import { cacheExchange } from '@urql/exchange-graphcache';
