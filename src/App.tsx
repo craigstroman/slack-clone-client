f66c19cfbc0e0c97135b4f9fc808ad2c8307864e
Craig Stroman <cstroman@gmail.com>
2026-08-17 16:48:36 -0400

    Removing todo comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-// TODO: Figure out why I am still getting errors with module.exports and also require, I have installed @types/node?
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
