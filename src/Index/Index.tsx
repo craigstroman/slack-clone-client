87d71c174592f38cb1f51ceb02526863cbe61cd2
Craig Stroman <cstroman@gmail.com>
2026-08-17 17:56:07 -0400

    Creating index component

---
+++ src/Index/Index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Layout } from '../components/Layout/Layout';
+
+export const Index: React.FC = () => {
+  return (
+    <Layout>
+      <h1>Slack Clone</h1>
+      <h2>Not a registered user? Sign up to create posts</h2>
+    </Layout>
+  );
+};
